8cc8c74a928acffdde536543e5085c081a4b04ea
Sean Conner <spc@conman.org>
2014-05-25 17:54:03 -0400

    Bug fix---Fix the _LEM file.

--- mktzip.lua
+++ mktzip.lua
@@ -14,7 +14,7 @@ NOTES   = [[
 A sample LEM file.  This isn't an application per se; it's more of a
 "proof-of-concept" and to show off some features of the file spec.
   	]]
-X_OTHER_DATA = true,
+X_OTHER_DATA = true
 ]=====]
 
 dofile "list.lua"
