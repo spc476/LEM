c066e20316ee1bd245bcb4b4cae6c7949a46a177
Sean Conner <spc@conman.org>
2014-06-13 21:36:00 -0400

    A list of files that comprises LuaRocks.

---
+++ lr.lua
@@ -0,0 +1,640 @@
+MODULES =
+{
+  [1] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.add",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/add.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [2] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.admin_remove",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/admin_remove.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [3] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.build",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/build.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [4] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.build.builtin",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/build/builtin.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [5] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.build.cmake",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/build/cmake.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [6] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.build.command",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/build/command.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [7] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.build.make",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/build/make.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [8] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.cache",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/cache.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [9] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.cfg",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/cfg.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [10] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.command_line",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/command_line.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [11] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.deps",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/deps.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [12] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.dir",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/dir.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [13] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.doc",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/doc.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [14] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.download",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/download.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [15] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [16] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch.cvs",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch/cvs.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [17] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch.git",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch/git.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [18] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch.git_file",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch/git_file.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [19] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch.hg",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch/hg.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [20] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch.sscm",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch/sscm.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [21] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fetch.svn",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fetch/svn.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [22] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fs",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fs.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [23] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fs.lua",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fs/lua.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [24] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fs.unix",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fs/unix.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [25] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fs.unix.tools",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fs/unix/tools.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [26] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fs.win32",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fs/win32.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [27] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.fs.win32.tools",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/fs/win32/tools.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [28] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.help",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/help.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [29] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.index",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/index.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [30] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.install",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/install.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [31] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.lint",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/lint.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [32] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.list",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/list.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [33] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.loader",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/loader.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [34] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.make",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/make.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [35] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.make_manifest",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/make_manifest.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [36] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.manif",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/manif.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [37] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.manif_core",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/manif_core.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [38] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.new_version",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/new_version.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [39] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.pack",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/pack.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [40] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.path",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/path.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [41] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.persist",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/persist.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [42] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.purge",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/purge.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [43] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.refresh_cache",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/refresh_cache.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [44] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.remove",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/remove.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [45] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.repos",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/repos.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [46] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.require",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/require.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [47] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.search",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/search.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [48] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.show",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/show.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [49] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.site_config",
+    contents = [[
+module("luarocks.site_config")
+LUAROCKS_PREFIX="/usr/local"
+LUA_INCDIR="/usr/local/include"
+LUA_LIBDIR="/usr/local/lib"
+LUA_BINDIR="/usr/local/bin"
+LUAROCKS_SYSCONFDIR="."
+LUAROCKS_ROCKS_TREE="/usr/local"
+LUAROCKS_UNAME_S="Linux"
+LUAROCKS_UNAME_M="i686"
+LUAROCKS_DOWNLOADER="wget"
+LUAROCKS_MD5CHECKER="md5sum"
+]],
+    lvmax = "5.2",
+    license = "MIT/X11",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [50] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.tools.patch",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/tools/patch.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [51] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.tools.tar",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/tools/tar.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [52] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.tools.zip",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/tools/zip.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [53] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.type_check",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/type_check.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [54] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.unpack",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/unpack.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [55] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.util",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/util.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [56] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.validate",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/validate.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [57] =
+  {
+    lvmin = "5.1",
+    name = "luarocks.write_rockspec",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/luarocks/write_rockspec.lua",
+    language = "Lua",
+    version = "2.1.2",
+  },
+}
+SCRIPTS =
+{
+  [1] =
+  {
+    lvmin = "5.1",
+    name = "config-5.1.lua",
+    contents = [[
+rocks_trees = {
+   home.."/.luarocks",
+   "/usr/local"
+}
+]],
+    language = "Lua",
+    lvmax = "5.1",
+  },
+  [2] =
+  {
+    lvmin = "5.1",
+    name = "config.lua",
+    contents = [[
+rocks_servers = {
+  "http://luarocks.org/repositories/rocks"
+}
+rocks_trees = {
+   home.."/.luarocks",
+   "/usr/local"
+}
+]],
+    language = "Lua",
+    lvmax = "5.2",
+  },
+}
+APP =
+{
+  [1] =
+  {
+    lvmin = "5.1",
+    name = "luarocks-admin",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/bin/luarocks-admin",
+    language = "Lua",
+    version = "2.1.2",
+  },
+  [2] =
+  {
+    lvmin = "5.1",
+    name = "luarocks",
+    license = "MIT/X11",
+    lvmax = "5.2",
+    file = "/tmp/luarocks-2.1.2/src/bin/luarocks",
+    language = "Lua",
+    version = "2.1.2",
+  },
+}
